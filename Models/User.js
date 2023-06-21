abbb6aad53ffd477b5f84b3694742e8c772f0677
gauravsahay007 <gauravsahay2468@gmail.com>
2023-06-21 15:00:23 +0530

    changes

--- Models/User.js
+++ Models/User.js
@@ -22,7 +22,7 @@ var userSchema = new Schema({
         required: true
     },
     salt:String,
-    pic:{
+    pic:{ 
         type: String,
         required: true,
         default:"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
